03628ad7b7135d0ea743a8b4ad1dcfd87ff200e1
Calvin Hawkes <calvinhawkes@gmail.com>
2012-04-08 00:09:32 -0400

    added wrapper

--- request_songs.php
+++ request_songs.php
@@ -1,16 +1,48 @@
 <?php
-
+include ("connection.php");
+include ("DateFilter.php");
+include ("GenreFilter.php");
 
 //song to be made smashed into JSON and sent through the interwebs
 class Song
 {
-    public $title;
-    public $artist;
-    public $ytcode;
+  public $title;
+  public $artist;
+  public $ytcode;
 }
 
+$songsPerPage = 30;
 
+$topOf = $_POST['timefilter'];
 $genre = $_POST['genrefilter'];
-$time = $_POST['timefilter'];
 
+$timeFilter = new DateFilter($topOf);
+$genreFilter = new GenreFilter($genre);
+
+$where = $timeFilter->genSQL() . ' AND ' . $genreFilter->genSQL();
+
+
+if($topOf == 'new') //newest was selected, order by upload date
+{
+  $qry = mysql_query("SELECT title,artist,youtubecode FROM  `songs` 
+                      WHERE $where
+                      ORDER BY uploaded_on DESC
+                      LIMIT 0 , $songsPerPage
+                      ");
+}
+else //order by score
+{
+  $qry = mysql_query("SELECT title,artist,youtubecode FROM  `songs` 
+                      WHERE $where
+                      ORDER BY score DESC
+                      LIMIT 0 , $songsPerPage
+                      ");
+}					
+if (!$qry)
+  die("FAIL: " . mysql_error());
+
+while($row = mysql_fetch_array($qry))
+{
+  //Do something
+}
 ?>
